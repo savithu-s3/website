13fab17da968cf86ee6c72177530a18eeb2720d8
Savithu_s3 <savithusapumal@gmail.com>
2022-09-09 06:29:26 +0530

    *

--- main.py
+++ main.py
@@ -6,8 +6,4 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/ind')
-def ind():
-    return render_template("ind.html")
-
 app.run(host="0.0.0.0", port="5050")
